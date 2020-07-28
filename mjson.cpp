8949f9de24eaec0d6561b644588430b3fb49f7b8
Anders Olme <anders.olme@gmail.com>
2020-07-28 15:37:06 +0200

    Added small test func
    aded a funtion to do some simple testing.

--- mjson.cpp
+++ mjson.cpp
@@ -2,13 +2,24 @@
 //
 
 #include <ctype.h>
-
+#include <iostream>
 #include "mjson.h"
 
 using namespace std;
 
+void check(bool result, std::string expected, std::string actual) {
+	if (result) {
+		std::cout << "\033[42m " << expected << "\033[m" << std::endl;
+	}
+	else {
+		std::cout << "Wanted: "<< expected <<" but got " << "\033[41m " << actual << "\033[m" << std::endl;
+	}
+}
+
 int main()
 {
-	cout << "Hello CMake." << endl;
+	check(true, "wanted", "got");
+	check(false, "wanted", "got");
+	
 	return 0;
 }
